1b1cea109ae5f853ceb0300fa866c9462e2365d7
Madison Youngblom <youngblom@wisc.edu>
2023-04-30 13:41:52 -0500

    biofilm

--- BEvA_DESeq.R
+++ BEvA_DESeq.R
@@ -25,30 +25,33 @@ sampleTable <- data.frame(SampleName = sampleData$LibraryName,
                           SampleID = sampleData$SampleID)
 
 # subset to only biofilm samples
-sampleTable <- sampleTable[sampleTable$Condition == "Biofilm",]
+B.table <- sampleTable[sampleTable$Condition == "Biofilm",]
 
 # create sample table for DESeq2 input
-DESeq2Table <- DESeqDataSetFromHTSeqCount(sampleTable = sampleTable, directory = ".",
+B.DESeq <- DESeqDataSetFromHTSeqCount(sampleTable = B.table, directory = ".",
                                           design = ~ Genotype+Clade)
 
 # remove rows with 0 counts
-DESeq2Table <- DESeq2Table[rowSums(counts(DESeq2Table)) > 1,]
-DESeq2Table <- DESeq(DESeq2Table)
-resultsNames(DESeq2Table)
-result <- results(DESeq2Table, alpha = 0.05, lfcThreshold = 0, contrast=c("Genotype","Evolved","Ancestral"))
-sum(result$padj <0.05, na.rm = TRUE)
-subSet <- subset(result,padj < 0.05) # Supplementary Data 3 - AllPopulations_BFEvA
+B.DESeq <- B.DESeq[rowSums(counts(B.DESeq)) > 1,]
+B.DESeq <- DESeq(B.DESeq)
+resultsNames(B.DESeq)
+BEvA.result <- results(B.DESeq, alpha = 0.05, lfcThreshold = 0, contrast=c("Genotype","Evolved","Ancestral"))
+write.csv(x = BEvA.result,"DEFiles/BEvA/all_BEvA.csv",quote=F)
+
+sum(BEvA.result$padj <0.05, na.rm = TRUE)
+subSet <- subset(BEvA.result,padj < 0.05) # Supplementary Data 3 - AllPopulations_BFEvA
+write.csv(x = subSet,"DEFiles/BEvA/all_BEvA_DE.csv",quote=F)
 
 # BF PCA with arrow
-rld <- rlogTransformation(DESeq2Table, blind=FALSE) # log transformation
-PCAdata <- DESeq2::plotPCA(rld, intgroup=c("Clade","Genotype"),returnData=T)
+B.rld <- rlogTransformation(B.DESeq, blind=FALSE) # log transformation
+PCAdata <- DESeq2::plotPCA(B.rld, intgroup=c("Clade","Genotype"),returnData=T)
 write.table(PCAdata,file="DEFiles/BEvA/BF_EvA_PCAdata.tsv",sep="\t")
 
 # PCA data file edited to connect ancestral and evolved samples
 PCAdata2 <- read.table("DEFiles/BEvA/BF_EvA_PCAdata_v2.txt",sep="\t",header=T)
-rv <- rowVars(assay(rld))
+rv <- rowVars(assay(B.rld))
 select <- order(rv, decreasing = TRUE)[seq_len(min(500,length(rv)))]
-pca <- prcomp(t(assay(rld)[select, ]))
+pca <- prcomp(t(assay(B.rld)[select, ]))
 percentVar <- pca$sdev^2/sum(pca$sdev^2)
 PC1var <- round(percentVar[1]*100)
 PC2var <- round(percentVar[2]*100)
@@ -63,14 +66,16 @@ PCAplot <- ggplot()+geom_point(data=PCAdata2,aes(x=PC1,y=PC2,color=Clade,shape=G
   ylim(-25,25)+theme_minimal()+xlab(paste0("PC1: ",PC1var,"%"))+ylab(paste0("PC2: ",PC2var,"%"))+
   scale_color_manual(name="Sub-lineage",labels=c("L4.9","L4.4"),breaks=c("L4.9","L4.4"),values=colors)+
   scale_shape_manual(name="Genotype",breaks=c("Ancestral","Evolved"), values=c(10,16))+
-  theme(legend.position = "top",axis.text=element_text(size=8),axis.title=element_text(size=10),
+  theme(legend.position = "top",axis.text=element_text(size=10),axis.title=element_text(size=12),
         legend.text=element_text(size=12))
 
 PCAplot
 
+ExportPlot(PCAplot,"../NewFigures/Figure5A",width=6,height=5)
+
 # separate analysis by lineage 
-L4.9.st <- sampleTable[sampleTable$Clade == "L4.9",]
-L4.4.st <- sampleTable[sampleTable$Clade == "L4.4",]
+L4.9.st <- B.table[B.table$Clade == "L4.9",]
+L4.4.st <- B.table[B.table$Clade == "L4.4",]
 
 # L4.9
 L4.9.t <- DESeqDataSetFromHTSeqCount(sampleTable = L4.9.st, directory = ".", design = ~ Genotype)
@@ -83,6 +88,7 @@ resultsNames(L4.9.t)
 L4.9.r <- results(L4.9.t, alpha = 0.05, lfcThreshold = 0, contrast=c("Genotype","Evolved","Ancestral"))
 sum(L4.9.r$padj <0.05, na.rm=TRUE)
 L4.9.subset <- subset(L4.9.r, padj<0.05) # Supplementary Data 3 - L4.9_BFEvA
+write.csv(L4.9.subset,"DEFiles/BEvA/L4.9_BEvA_DE.csv",quote=F)
 
 # L4.4
 L4.4.t <- DESeqDataSetFromHTSeqCount(sampleTable = L4.4.st, directory = ".", design = ~ Genotype)
@@ -95,7 +101,7 @@ resultsNames(L4.4.t)
 L4.4.r <- results(L4.4.t, alpha = 0.05, lfcThreshold = 0, contrast=c("Genotype","Evolved","Ancestral"))
 sum(L4.4.r$padj <0.05, na.rm=TRUE)
 L4.4.subset <- subset(L4.4.r, padj<0.05) # Supplementary Data 3 - L4.4_BFEvA
-
+write.csv(L4.4.subset,"DEFiles/BEvA/L4.4_BEvA_DE.csv",quote=F)
 
 # volcano plot - L4.9 & L4.4 data plotted together (Figure 5D)
 colors <- c(plasma(2,begin=0.4,end = 0.8))
@@ -116,13 +122,17 @@ all.voldata <- all.voldata %>% mutate(sig=case_when(padj > 0.05 ~ "NS",
 vol.sub <- all.voldata[all.voldata$sig %in% c("L4.4","L4.9","NS"),]
 
 vol <- ggplot(vol.sub,aes(x=lfc,y=-log10(padj))) + 
-  geom_point(aes(color=sig),alpha=0.5,size=2) +
+  geom_point(aes(color=sig),alpha=0.75,size=2) +
   theme_minimal()+theme(legend.position = "top")+
   scale_color_manual(name="Sub-lineage",labels=c("Both","L4.9","L4.4","NS"),breaks=c("Both","L4.9","L4.4","NS"),values=c("#D76863",colors,"grey"))+
   geom_vline(xintercept=0,linetype="dashed",color="grey27",alpha=0.8) + xlim(-5,5) + ylim(0,8)+
-  xlab("log2 Fold Change")
+  xlab("log2 Fold Change") +
+  theme(legend.position = "top",axis.text=element_text(size=10),axis.title=element_text(size=12),
+        legend.text=element_text(size=12))
 
 vol
+
+ExportPlot(vol,"../NewFigures/Figure5D",width=6,height=6)
 
 # barplot of up/downregulated genes specific to each lineage (Figure 5C)
 
@@ -140,50 +150,53 @@ bar.data <- data.frame(dir=rep(c("up","down"),3), lin=c("L4.4","L4.4","L4.9","L4
 
 count.plot <- ggplot(bar.data,(aes(x=lin,y=count))) + geom_col(aes(fill=lin,alpha=dir),width=0.5)+ theme_bw()+
   scale_fill_manual(name="Sub-lineage",values=c("#FCA636FF","#B12A90FF","#D76863"),labels=c("L4.4","L4.9","Shared"),breaks=c("L4.4","L4.9","Shared"))+
-  scale_alpha_manual(name="DEGs",values=c(0.4,1),labels=c("downregulated","upregulated"),breaks=c("down","up"))+
+  scale_alpha_manual(name=NULL,values=c(0.4,1),labels=c("downregulated","upregulated"),breaks=c("down","up"))+
   xlab(NULL)+ylab("Count")+
   guides(fill=guide_legend(order=1))+
   theme(axis.text=element_text(size=12),axis.title.y = element_text(size=12))
 count.plot
 
+ExportPlot(count.plot,"../NewFigures/Figure5C",width=6,height=6)
+
 # separate by individual strains (Supplementary Data 3 - BFEvA for each strain)
 strains <- c("31","49","55","72","345","540")
 
 for (strain in strains){
-  st <- sampleTable[sampleTable$Strain == strain,]
+  st <- B.table[B.table$Strain == strain,]
   t <- DESeqDataSetFromHTSeqCount(sampleTable = st, directory = ".", design = ~ Genotype)
   t <- t[rowSums(counts(t)) > 1,]
   t <- DESeq(t)
   r <- results(t, alpha = 0.05, lfcThreshold = 0, contrast=c("Genotype","Evolved","Ancestral"))
   sub <- subset(r, padj < 0.05)
-  write.csv(sub,paste("../2023.02.28_newFigs/DEFiles/",paste(strain,"BFEvA",sep="_"),".csv",sep=""))
+  write.csv(sub,paste("DEFiles/BEvA/",paste(strain,"BFEvA",sep="_"),".csv",sep=""),quote=F)
 }
 
 # l2fc values by feature type (Figure 8B)
 ncrna <- read.delim("Metadata/all_ncRNA.txt",header=F)$V1
 sorfs <- read.delim("Metadata/all_sORFs.txt",header=F)$V1
-result <- data.frame(result)
-result$gene <- rownames(result)
+BEvA.result <- data.frame(BEvA.result)
+BEvA.result$gene <- rownames(BEvA.result)
 
-result <- result %>% mutate(type=case_when(gene %in% ncrna ~ "ncRNA",
+BEvA.result <- BEvA.result %>% mutate(type=case_when(gene %in% ncrna ~ "ncRNA",
                                            gene %in% sorfs ~ "sORF",
                                            TRUE ~ "ORF"))
 my_colors <- magma(n=4, end = 0.8, begin=0.25)
 feature_colors <- c(my_colors[c(2)],"forestgreen",my_colors[c(4)])
 
-plot <- ggplot(result,aes(x=type,y=log2FoldChange)) + geom_hline(yintercept=0,linetype="dashed")+
+plot <- ggplot(BEvA.result,aes(x=type,y=log2FoldChange)) + geom_hline(yintercept=0,linetype="dashed")+
   geom_boxplot(aes(fill=type),alpha=0.8)+
   theme_minimal() + xlab(NULL)+ ylab("Log2 Fold Change")+
   scale_fill_manual(name=NULL,values=feature_colors,breaks=c("ncRNA","ORF","sORF"))+
   theme(axis.text=element_text(size=12,face="bold"),
         axis.text.x = element_blank(),
         axis.title = element_text(size=12),
-        strip.text = element_text(size=12,face="bold"))
+        strip.text = element_text(size=12,face="bold"))+
+  scale_y_continuous(limits=c(-13,6),breaks=c(-10,-5,0,5))
 plot
 
-stat <- compare_means(log2FoldChange ~ type, data = result,p.adjust.method = "BH")
+stat <- compare_means(log2FoldChange ~ type, data = BEvA.result,p.adjust.method = "BH")
 stat <- stat %>% mutate(y.position=c(5,NA,6))
 box.stats <- plot + stat_pvalue_manual(stat, label="p.signif",label.size=4)
 box.stats
 
-ExportPlot(box.stats,"../2023.02.28_newFigs/IndvFigures/BEvA_featuretype",width=6,height=6)
+ExportPlot(box.stats,"../NewFigures/Figure8B",width=6,height=6)
